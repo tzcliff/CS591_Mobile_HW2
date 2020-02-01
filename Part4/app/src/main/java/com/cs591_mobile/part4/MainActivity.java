fbe9c321012e80df43770ff468053cc241faa9b6
宋彦杉 <songyanshan.2008@163.com>
2020-02-01 15:04:58 -0500

    test

--- Part4/app/src/main/java/com/cs591_mobile/part4/MainActivity.java
+++ Part4/app/src/main/java/com/cs591_mobile/part4/MainActivity.java
@@ -1,11 +1,11 @@
 package com.cs591_mobile.part4;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
